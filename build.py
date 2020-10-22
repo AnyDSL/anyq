4d6bcee00dfa840a8055ebf635db199a45cd1e4d
Michael Kenzel <kenzel@cg.uni-saarland.de>
2020-10-22 15:59:11 +0200

    fixed build scripts, added boost

--- build.py
+++ build.py
@@ -10,6 +10,51 @@ def _git_apply_patch(dir, patch):
 		git("-C", dir, "apply", "-v", patch)
 
 @component()
+class Boost:
+	def __init__(self, dir, buildsystem):
+		self.source_dir = dir/"boost"
+		self.build_dir = self.source_dir/"build"
+
+	def pull(self):
+		pull_git_dependency(self.source_dir, "https://github.com/boostorg/boost.git", branch="boost-1.72.0")
+		git("-C", self.source_dir,"submodule", "update", "--init",
+			"tools", "libs/config", "libs/predef", "libs/headers", "libs/core", "libs/detail",
+			"libs/type_traits",
+			"libs/preprocessor",
+			"libs/assert",
+			"libs/static_assert",
+			"libs/mpl",
+			"libs/iterator",
+			"libs/move",
+			"libs/smart_ptr",
+			"libs/intrusive",
+			"libs/functional",
+			"libs/container_hash",
+			"libs/context",
+			"libs/system",
+			"libs/winapi",
+			"libs/io",
+			"libs/filesystem",
+			"libs/fiber",
+			"libs/context",
+			"libs/filesystem"
+		)
+
+	def configure(self):
+		if (self.source_dir/"b2.exe").exists():
+			return
+
+		if cmd("cmd", "/C", "bootstrap.bat", cwd=self.source_dir) != 0:
+			raise Exception("failed to bootstrap boost")
+
+	def remove(self):
+		rmtree(self.build_dir)
+
+	def build(self):
+		if cmd(self.source_dir/"b2", "install", f"--prefix={self.build_dir}", "--with-fiber", cwd=self.source_dir) != 0:
+			raise Exception("failed to build boost")
+
+@component()
 class AnyDSL(Build):
 	def __init__(self, dir, buildsystem):
 		super().__init__(buildsystem)
@@ -26,8 +71,8 @@ class LLVM(Build):
 		self.rv_source_dir = self.source_dir/"rv"
 
 	def pull(self):
-		pull_git_dependency(self.source_dir, "https://github.com/llvm/llvm-project.git", branch="llvmorg-11.0.0-rc2")
-		pull_git_dependency(self.rv_source_dir, "https://github.com/cdl-saarland/rv.git", "--recurse-submodules")
+		pull_git_dependency(self.source_dir, "https://github.com/llvm/llvm-project.git", branch="llvmorg-11.0.0")
+		pull_git_dependency(self.rv_source_dir, "https://github.com/cdl-saarland/rv.git", "--recurse-submodules", branch="release/11.x")
 
 	def configure(self, anydsl):
 		patches = [
@@ -54,7 +99,8 @@ class LLVM(Build):
 			 "-DLLVM_TOOL_LLD_BUILD=ON",
 			 "-DLLVM_TOOL_RV_BUILD=ON",
 			 "-DCLANG_INCLUDE_DOCS=OFF",
-			 "-DCLANG_INCLUDE_TESTS=OFF"
+			 "-DCLANG_INCLUDE_TESTS=OFF",
+			 "-DLLVM_RVPLUG_LINK_INTO_TOOLS:BOOL=ON"
 		)
 
 	def build(self):
@@ -68,7 +114,7 @@ class Thorin(Build):
 		self.half_source_dir = self.source_dir/"half"
 
 	def pull(self):
-		pull_git_dependency(self.source_dir, "https://github.com/AnyDSL/thorin.git", branch="artic+llvm/11.x")
+		pull_git_dependency(self.source_dir, "https://github.com/AnyDSL/thorin.git", branch="llvm/11.x")
 		pull_svn_dependency(self.half_source_dir, "https://svn.code.sf.net/p/half/code/tags/release-1.11.0")
 
 	def configure(self, llvm):
@@ -77,7 +123,8 @@ class Thorin(Build):
 			f"-DLLVM_DIR:PATH={llvm.build_dir/'lib'/'cmake'/'llvm'}",
 			f"-DRV_INCLUDE_DIR:PATH={llvm.rv_source_dir/'include'}",
 			f"-DRV_LIBRARY:PATH={llvm.build_dir/'lib'/'RV.lib'}",
-			f"-DRV_SLEEF_LIBRARY:PATH={llvm.build_dir/'lib'/'gensleef.lib'}",
+			# f"-DRV_SLEEF_LIBRARY:PATH={llvm.build_dir/'lib'/'gensleef.lib'}",
+			f"-DRV_SLEEF_LIBRARY:PATH=",
 			f"-DHalf_INCLUDE_DIR:PATH={self.half_source_dir/'include'}",
 			 "-DBUILD_SHARED_LIBS=OFF"
 		)
@@ -172,23 +219,24 @@ class LPNG(MultiConfigBuild):
 	def build_config(self, build_dir, build_type):
 		ninja("-C", str(build_dir), "install")
 
-@component(depends_on=(ZLIB, LPNG, Thorin, Artic, AnyDSLRuntime))
+@component(depends_on=(ZLIB, LPNG, Boost, Thorin, Artic, AnyDSLRuntime))
 class AnyQ(Build):
 	def __init__(self, dir, buildsystem):
 		super().__init__(buildsystem, dir/"build")
 		self.source_dir = dir
 
-	def configure(self, zlib, libpng, thorin, artic, runtime):
+	def configure(self, zlib, libpng, boost, thorin, artic, runtime):
 		self.buildsystem.configure(self.build_dir, self.source_dir,
 			f"-DZLIB_ROOT={zlib.install_dir}",
 			f"-DPNG_ROOT={libpng.install_dir}",
+			f"-DBoost_ROOT={boost.build_dir}",
 			# f"-DThorin_DIR:PATH={thorin.build_dir/'share'/'anydsl'/'cmake'}",
 			f"-DArtic_BINARY_DIR:PATH={artic.build_dir/'bin'}",
 			f"-DAnyDSL_runtime_DIR={runtime.build_dir/'share'/'anydsl'/'cmake'}"
 		)
 
 
-dependency_name_map = { "zlib": ZLIB, "libpng": LPNG, "llvm": LLVM, "thorin": Thorin, "artic": Artic, "runtime": AnyDSLRuntime, "anydsl": AnyDSL }
+dependency_name_map = { "boost": Boost, "zlib": ZLIB, "libpng": LPNG, "llvm": LLVM, "thorin": Thorin, "artic": Artic, "runtime": AnyDSLRuntime, "anydsl": AnyDSL }
 
 def lookup_dependency(name):
 	d = dependency_name_map.get(name)
@@ -214,17 +262,23 @@ def main(args):
 
 	if args.remove:
 		for c in components:
+			print("--------------------------------")
 			c.remove()
+			print()
 		return
 
 	if args.pull:
 		for c in components:
+			print("--------------------------------")
 			c.pull()
+			print()
 		return
 
 	for c in components:
+		print("--------------------------------")
 		c.configure(*c.dependencies)
 		c.build()
+		print()
 
 	copy_dlls(this_dir/"build"/"bin"/"Debug", components, "debug_dlls")
 	copy_dlls(this_dir/"build"/"bin"/"Release", components, "release_dlls")
