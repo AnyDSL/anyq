e970063f4c48187181b9bd9802d124465d013806
Stefan Lemme <stefan.lemme@dfki.de>
2021-11-23 14:25:19 +0100

    Revision of perform-cpu-test.py to take ISA into account

--- src/perform-cpu-test.py
+++ src/perform-cpu-test.py
@@ -8,7 +8,7 @@ instructions = [
     'shufflevector'
 ]
 
-prefix = '//inst_stats:'
+prefix = re.compile('^//inst_stats\[([a-z0-9_\-]+)\]:')
 srcstats = re.compile('([a-z0-9\._]+)\(([0-9]+)\),?')
 
 if __name__ == "__main__":
@@ -33,31 +33,33 @@ if __name__ == "__main__":
         print(result.stdout)
         sys.exit(result.returncode)
 
-    inst_stats = dict([(inst, 0) for inst in instructions])
+    inst_stats = dict()
     found_inst_stats = False
 
     with open(args.src_file, 'r', encoding='utf-8') as file:
         for line in file:
             line = line.strip().replace(' ', '').replace('\t', '')
-            if not line.startswith(prefix):
+
+            m = prefix.match(line)
+            if m is None:
                 continue
 
-            line = line[len(prefix):]
-            # print("Found inst_stats:", line)
+            isa = m.group(1)
+            inst_stats[isa] = dict([(inst, 0) for inst in instructions])
+
+            remaining_line = line[len(m.group(0)):]
+            # print("Found inst_stats:", remaining_line)
+            # print("for isa", isa)
             found_inst_stats = True
 
-            for m in srcstats.finditer(line):
-                inst_stats[m.group(1)] = int(m.group(2))
+            for m in srcstats.finditer(remaining_line):
+                inst_stats[isa][m.group(1)] = int(m.group(2))
 
     if not found_inst_stats:
         sys.exit(0)
 
     # print()
     # print("inst_stats:", inst_stats)
-
-    current_stats = {}
-    for key in inst_stats.keys():
-        current_stats[key] = 0
 
     llvm_file = args.llvm_file
     if not args.llvm_file.is_file():
@@ -67,27 +69,56 @@ if __name__ == "__main__":
         print("llvm file not found:", args.llvm_file)
         sys.exit(-2)
 
+    current_isa = None
+    current_stats = {}
+    compare_inst_stats = None
+
     with open(llvm_file, 'r', encoding='utf-8') as file:
+        # parse preamble
         for line in file:
-            if line.lstrip().startswith('declare'):
+            line = line.lstrip()
+            if line.startswith(';'):
                 continue
 
-            if line.lstrip().startswith('define'):
+            if line.startswith('source_filename'):
                 continue
 
-            if line.lstrip().startswith(';'):
+            target_prefix = 'target triple = "'
+            if line.startswith(target_prefix):
+                for target in inst_stats.keys():
+                    if line[len(target_prefix):].startswith(target):
+                        current_isa = target
+                        # print('Found target isa is', current_isa)
+                if current_isa is not None:
+                    break
+
+            if line.startswith('target'):
                 continue
 
-            if line.lstrip().startswith('!'):
+        # no matching inst_stats found
+        if current_isa is None:
+            sys.exit(0)
+
+        compare_inst_stats = inst_stats[current_isa]
+        for key in compare_inst_stats.keys():
+            current_stats[key] = 0
+
+        # process remaining file
+        for line in file:
+            line = line.lstrip()
+            if line.startswith('declare'):
                 continue
 
-            if line.lstrip().startswith('source_filename'):
+            if line.startswith('define'):
                 continue
 
-            if line.lstrip().startswith('target'):
+            if line.startswith(';'):
                 continue
 
-            for key in inst_stats.keys():
+            if line.startswith('!'):
+                continue
+
+            for key in compare_inst_stats.keys():
                 if key not in line:
                     continue
 
@@ -96,8 +127,9 @@ if __name__ == "__main__":
     # print("current_stats:", current_stats)
 
     passed = True
-    for key in inst_stats.keys():
-        passed = current_stats[key] == inst_stats[key]
+
+    for key in compare_inst_stats.keys():
+        passed = current_stats[key] == compare_inst_stats[key]
         if not passed:
             break
 
@@ -106,12 +138,12 @@ if __name__ == "__main__":
     else:
         print('failed with wrong instructions!')
 
-    for key in inst_stats.keys():
-        if current_stats[key] != inst_stats[key]:
+    for key in compare_inst_stats.keys():
+        if current_stats[key] != compare_inst_stats[key]:
             print('(!)', end=' ')
         else:
             print('   ', end=' ')
-        print(key, '(', inst_stats[key], ') ->', current_stats[key])
+        print(key, '(', compare_inst_stats[key], ') ->', current_stats[key])
 
     if not passed:
         sys.exit(-1)
