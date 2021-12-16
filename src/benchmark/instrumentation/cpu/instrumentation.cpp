e0e9bb1aba2199636e6ebdb3a26f65f0cce53d2d
Michael Kenzel <michael.kenzel@gmail.com>
2021-12-16 14:00:11 +0100

    use proper device info from anydsl runtime (completes #65)

---
+++ src/benchmark/instrumentation/cpu/instrumentation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <anydsl_runtime.hpp>
+
+#include "instrumentation.h"
+
+
+std::ostream& Instrumentation::print_device_info(std::ostream& out)
+{
+	return out << anydsl_device_name(anydsl::make_device(anydsl::Platform::Host, anydsl::Device(device)));
+}
