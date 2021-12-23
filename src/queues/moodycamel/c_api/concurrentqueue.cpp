0291e85de84e3c74cb6ce84d9952091d976e77bc
Stefan Lemme <stefan.lemme@dfki.de>
2021-12-23 17:34:07 +0100

    Optimize moodycamel::ConcurrentQueue[u32] as IndexQueue

--- src/queues/moodycamel/c_api/concurrentqueue.cpp
+++ src/queues/moodycamel/c_api/concurrentqueue.cpp
@@ -1,13 +1,35 @@
 #include "concurrentqueue.h"
 #include "../concurrentqueue.h"
 
-typedef moodycamel::ConcurrentQueue<void*> MoodycamelCQType, *MoodycamelCQPtr;
+#include <iostream>
+
+
+static bool locked_memory = false;
+
+struct ConcurrentQueueTraits : public moodycamel::ConcurrentQueueDefaultTraits {
+public:
+	ConcurrentQueueTraits() { locked_memory = false; }
+
+	static void* malloc(size_t size) {
+		//std::cout << "malloc(" << size << ")" << std::endl;
+		//if (locked_memory)
+		//	return nullptr;
+		return moodycamel::ConcurrentQueueDefaultTraits::malloc(size);
+	}
+	static inline void free(void* ptr) { return moodycamel::ConcurrentQueueDefaultTraits::free(ptr); }
+};
+
+typedef moodycamel::ConcurrentQueue<void*, ConcurrentQueueTraits> MoodycamelCQType, *MoodycamelCQPtr;
+typedef moodycamel::ConcurrentQueue<uint32_t, ConcurrentQueueTraits> MoodycamelCQU32Type, *MoodycamelCQU32Ptr;
 
 extern "C" {
 
-int moodycamel_cq_create(MoodycamelCQHandle* handle)
+int moodycamel_cq_create(uint32_t capacity, uint32_t num_producer, MoodycamelCQHandle* handle)
 {
-	MoodycamelCQPtr retval = new MoodycamelCQType;
+	//locked_memory = false;
+	MoodycamelCQPtr retval = new MoodycamelCQType(/*capacity*/ 6 * MoodycamelCQType::BLOCK_SIZE, 0, num_producer);
+	//locked_memory = true;
+
 	if (retval == nullptr) {
 		return 0;
 	}
@@ -34,6 +56,41 @@ int moodycamel_cq_try_dequeue(MoodycamelCQHandle handle, MoodycamelValue* value)
 size_t moodycamel_cq_size_approx(MoodycamelCQHandle handle)
 {
     return reinterpret_cast<MoodycamelCQPtr>(handle)->size_approx();
+}
+
+
+
+int moodycamel_cq_create_u32(uint32_t capacity, uint32_t num_producer, MoodycamelCQHandle* handle)
+{
+	//locked_memory = false;
+	MoodycamelCQU32Ptr retval = new MoodycamelCQU32Type(/*capacity*/ 6 * MoodycamelCQU32Type::BLOCK_SIZE, 0, num_producer);
+	//locked_memory = true;
+	if (retval == nullptr) {
+		return 0;
+	}
+	*handle = retval;
+	return 1;
+}
+
+int moodycamel_cq_destroy_u32(MoodycamelCQHandle handle)
+{
+	delete reinterpret_cast<MoodycamelCQU32Ptr>(handle);
+	return 1;
+}
+
+int moodycamel_cq_enqueue_u32(MoodycamelCQHandle handle, uint32_t value)
+{
+	return reinterpret_cast<MoodycamelCQU32Ptr>(handle)->enqueue(value) ? 1 : 0;
+}
+
+int moodycamel_cq_try_dequeue_u32(MoodycamelCQHandle handle, uint32_t* value)
+{
+	return reinterpret_cast<MoodycamelCQU32Ptr>(handle)->try_dequeue(*value) ? 1 : 0;
+}
+
+uint64_t moodycamel_cq_size_approx_u32(MoodycamelCQHandle handle)
+{
+	return reinterpret_cast<MoodycamelCQU32Ptr>(handle)->size_approx();
 }
 
 }
--- src/queues/moodycamel/c_api/concurrentqueue.h
+++ src/queues/moodycamel/c_api/concurrentqueue.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -24,11 +25,17 @@ typedef void* MoodycamelCQHandle;
 typedef void* MoodycamelBCQHandle;
 typedef void* MoodycamelValue;
 
-MOODYCAMEL_EXPORT int moodycamel_cq_create(MoodycamelCQHandle* handle);
+MOODYCAMEL_EXPORT int moodycamel_cq_create(uint32_t capacity, uint32_t num_producer, MoodycamelCQHandle* handle);
 MOODYCAMEL_EXPORT int moodycamel_cq_destroy(MoodycamelCQHandle handle);
 MOODYCAMEL_EXPORT int moodycamel_cq_enqueue(MoodycamelCQHandle handle, MoodycamelValue value);
 MOODYCAMEL_EXPORT int moodycamel_cq_try_dequeue(MoodycamelCQHandle handle, MoodycamelValue* value);
 MOODYCAMEL_EXPORT size_t moodycamel_cq_size_approx(MoodycamelCQHandle handle);
+
+MOODYCAMEL_EXPORT int moodycamel_cq_create_u32(uint32_t capacity, uint32_t num_producer, MoodycamelCQHandle* handle);
+MOODYCAMEL_EXPORT int moodycamel_cq_destroy_u32(MoodycamelCQHandle handle);
+MOODYCAMEL_EXPORT int moodycamel_cq_enqueue_u32(MoodycamelCQHandle handle, uint32_t value);
+MOODYCAMEL_EXPORT int moodycamel_cq_try_dequeue_u32(MoodycamelCQHandle handle, uint32_t* value);
+MOODYCAMEL_EXPORT uint64_t moodycamel_cq_size_approx_u32(MoodycamelCQHandle handle);
 
 MOODYCAMEL_EXPORT int moodycamel_bcq_create(MoodycamelBCQHandle* handle);
 MOODYCAMEL_EXPORT int moodycamel_bcq_destroy(MoodycamelBCQHandle handle);
