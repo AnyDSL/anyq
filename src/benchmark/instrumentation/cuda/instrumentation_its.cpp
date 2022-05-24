e61cabc619a6247b83aaa19b05a9898f1e0a77bc
Michael Kenzel <michael.kenzel@gmail.com>
2022-05-24 11:23:22 +0200

    fail if ITS not supported

--- src/benchmark/instrumentation/cuda/instrumentation_its.cpp
+++ src/benchmark/instrumentation/cuda/instrumentation_its.cpp
@@ -18,8 +18,8 @@ Instrumentation::Instrumentation(int device_id)
 	int cc_major;
 	throw_error(cuDeviceGetAttribute(&cc_major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, device));
 
-	// if (cc_major < 7)
-	// 	throw std::runtime_error("ITS not supported on this device");
+	if (cc_major < 7)
+		throw std::runtime_error("ITS not supported on this device");
 
 	init_profiler();
 }
