5c7159181483cd5fc7377018ec87bc81d74684a9
Richard Membarth <richard.membarth@uni-saarland.de>
2022-09-21 16:45:07 +0200

    Fix for clang

--- src/queues/YMCQ/wfqueue/primitives.h
+++ src/queues/YMCQ/wfqueue/primitives.h
@@ -3,7 +3,7 @@
 #ifndef PRIMITIVES_H
 #define PRIMITIVES_H
 
-#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 7))
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 7)) || defined(__clang__)
 /**
  * An atomic fetch-and-add.
  */
