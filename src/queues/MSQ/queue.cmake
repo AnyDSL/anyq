d8767641cf04aee5ec83c75a44b9d933119bb5d7
Michael Kenzel <michael.kenzel@gmail.com>
2023-11-24 06:17:09 +0100

    play around with atomic optimizations

--- src/queues/MSQ/queue.cmake
+++ src/queues/MSQ/queue.cmake
@@ -1,3 +1,5 @@
 set(MichaelScottQueue_short_name MSQ)
+set(MichaelScottQueueOpt_short_name MSQ_opt)
 
 set(MichaelScottQueue_sources ${CMAKE_CURRENT_LIST_DIR}/michael_scott.art)
+set(MichaelScottQueueOpt_sources ${CMAKE_CURRENT_LIST_DIR}/michael_scott.art)
